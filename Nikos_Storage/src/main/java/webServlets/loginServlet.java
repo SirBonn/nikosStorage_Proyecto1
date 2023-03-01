d174ddb67c4d3fc8e6c297a1a88a0bed11ae3b44
SirBonn <byronvasquez21@gmail.com>
2023-02-28 22:34:21 -0600

    packete de servlets para funcionamiento de la web

---
+++ Nikos_Storage/src/main/java/webServlets/loginServlet.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webServlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+/**
+ *
+ * @author sirbon
+ */
+@WebServlet("/loginServlet")
+public class loginServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //leemos los parametros 
+        
+        String usuario = request.getParameter("usuario");
+        String pasword = request.getParameter("password");
+        
+        System.out.println("usuario" + usuario);
+        System.out.println("password" + pasword);
+        
+        PrintWriter out = response.getWriter();
+        
+        out.print("<html>");
+        out.print("<body>");
+        out.print("el parametro usuario es " + usuario);
+        out.print("</br>");
+        out.print("el parametro contraseña es " +pasword);
+        out.print("</br>");
+        out.print("</body>");
+        out.print("</html>");
+        out.close();
+        
+    }
+
+}
