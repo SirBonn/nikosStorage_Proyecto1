f9876478f83b7e00f3fe951c2f942d2a0772f5cb
SirBonn <byronvasquez21@gmail.com>
2023-03-06 02:29:15 -0600

    se creo servlet para control de inicio de sesiones

--- Nikos_Storage/src/main/java/webServlets/loginServlet.java
+++ Nikos_Storage/src/main/java/webServlets/loginServlet.java
@@ -5,12 +5,13 @@
  */
 package webServlets;
 
+import dataBase.UsuarioDAO;
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
-import Security.PasswordEncoder;
+import dominio.*;
+import javax.servlet.RequestDispatcher;
 
 /**
  *
@@ -22,25 +23,35 @@ public class loginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //leemos los parametros 
-        
-        String usuario = request.getParameter("usuario");
+
+        String nick = request.getParameter("usuario");
         String pasword = request.getParameter("password");
-        String encodePswrd = PasswordEncoder.encodePassword(pasword);
-        System.out.println("usuario" + usuario);
-        System.out.println("password" + encodePswrd);
-        
-        PrintWriter out = response.getWriter();
-        
-        out.print("<html>");
-        out.print("<body>");
-        out.print("el parametro usuario es " + usuario);
-        out.print("</br>");
-        out.print("el parametro contraseña es " +encodePswrd);
-        out.print("</br>");
-        out.print("</body>");
-        out.print("</html>");
-        out.close();
-        
+
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+        Usuario loggedUsuario = usuarioDAO.logearUsuario(new Usuario(nick, pasword));
+        HttpSession session = request.getSession();
+        session.setAttribute(nick, loggedUsuario.getPassword());
+
+        if (loggedUsuario.getLevelUsr() == 0) {
+
+            response.sendRedirect("gestion-administrativa.jsp");
+
+        } else if (loggedUsuario.getLevelUsr() == 1) {
+            response.sendRedirect("gestion-tienda.jsp");
+
+        } else if (loggedUsuario.getLevelUsr() == 2) {
+            response.sendRedirect("gestion-bodeguera.jsp");
+
+        } else if (loggedUsuario.getLevelUsr() == 3) {
+            response.sendRedirect("gestion-supervisora.jsp");
+
+        } else if (loggedUsuario.getLevelUsr() < 0) {
+            request.setAttribute("error", "Usuario o contraseña incorrectos");
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/login.jsp");
+            dispatcher.forward(request, response);
+
+        }
+
     }
 
 }
