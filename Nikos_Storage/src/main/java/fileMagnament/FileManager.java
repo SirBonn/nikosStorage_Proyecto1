d632805498f9e854aa3f3b4ee905f446b89e61e4
SirBonn <byronvasquez21@gmail.com>
2023-03-11 00:34:11 -0600

    procesamiento explicito de archivo JSON

--- Nikos_Storage/src/main/java/fileMagnament/FileManager.java
+++ Nikos_Storage/src/main/java/fileMagnament/FileManager.java
@@ -37,9 +37,9 @@ public class FileManager {
         } catch (JsonProcessingException ex) {
             Logger.getLogger(FileManager.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
-            
+
             Logger.getLogger(FileManager.class.getName()).log(Level.SEVERE, null, ex);
-            
+
         }
 
     }
@@ -50,7 +50,10 @@ public class FileManager {
         cargarProductos();
         cargarTiendas();
         cargarUsuarios();
-
+        cargarPedidos();
+        cargarEnvios();
+        cargarDevoluciones();
+        cargarIncidencias();
         return rowsAffected;
     }
 
@@ -61,9 +64,9 @@ public class FileManager {
 
         //supervisores
         ArrayList<Supervisor> supervisores = new ArrayList<>();
-        JsonNode supervisorNode = rootNode.get("supervisores");
+        JsonNode usuariosJsonNode = rootNode.get("supervisores");
 
-        for (JsonNode usuarioJsonNode : supervisorNode) {
+        for (JsonNode usuarioJsonNode : usuariosJsonNode) {
 
             int codigo = usuarioJsonNode.get("codigo").asInt();
             String nombre = usuarioJsonNode.get("nombre").asText();
@@ -78,27 +81,25 @@ public class FileManager {
 
             rowsAffected += usuarioDAO.insertUsuario(sup, "SUPERVISORES");
         }
-        
+
         //Administradores
-        ArrayList<Usuario> usuarios = new ArrayList<>();
-        JsonNode usuariosJsonNode = rootNode.get("admins");
+        ArrayList<Usuario> admins = new ArrayList<>();
+        JsonNode adminsNode = rootNode.get("admins");
 
-        for (JsonNode usuarioJsonNode : usuariosJsonNode) {
+        for (JsonNode usuarioJsonNode : adminsNode) {
 
-            int codigo = usuarioJsonNode.get("codigo").asInt();
             String nombre = usuarioJsonNode.get("nombre").asText();
             String username = usuarioJsonNode.get("username").asText();
             String password = usuarioJsonNode.get("password").asText();
-            String email = usuarioJsonNode.get("email").asText();
-            Usuario usuario = new Usuario(codigo, nombre, username, password, email);
-            usuarios.add(usuario);
+            Usuario admin = new Usuario(nombre, username, password);
+            admins.add(admin);
 
         }
-        for (Usuario usr : usuarios) {
+        for (Usuario adm : admins) {
 
-            rowsAffected += usuarioDAO.insertUsuario(usr, "SUPERVISORES");
+            rowsAffected += usuarioDAO.insertUsuario(adm, "ADMINISTRADORES");
         }
-        
+
         //Dependientes
         ArrayList<Dependiente> dependientes = new ArrayList<>();
         JsonNode dependientesNode = rootNode.get("usuariostienda");
@@ -152,7 +153,7 @@ public class FileManager {
     private int cargarTiendas() {
 
         TiendaDAO tiendaDAO = new TiendaDAO();
-
+        CatalogoDAO catalogoDAO = new CatalogoDAO();
         int rowsAffected = 0;
 
         ArrayList<Tienda> tiendas = new ArrayList<>();
@@ -163,15 +164,15 @@ public class FileManager {
             int codigoTda = tiendaNode.get("codigo").asInt();
             String direccion = tiendaNode.get("direccion").asText();
             String tipo = tiendaNode.get("tipo").asText();
-            JsonNode productos = tiendaNode.get("productos");
+            JsonNode catalogo = tiendaNode.get("productos");
             Tienda tienda = new Tienda(codigoTda, direccion, tipo);
             tienda.crearCatalogo();
-            for (JsonNode productosArray : productos) {
-                int codigoPdc = productosArray.get("codigo").asInt();
-                int existenciasPdc = productosArray.get("existencias").asInt();
+            for (JsonNode catArray : catalogo) {
 
-                Producto producto = new Producto(codigoPdc, existenciasPdc);
-                tienda.agregarAlCatalogo(producto);
+                int codigoPdc = catArray.get("codigo").asInt();
+                int existenciasPdc = catArray.get("existencias").asInt();
+
+                tienda.agregarAlCatalogo(new Producto(codigoPdc, existenciasPdc));
             }
             tiendas.add(tienda);
 
@@ -179,6 +180,7 @@ public class FileManager {
         for (Tienda td : tiendas) {
 
             rowsAffected += tiendaDAO.insertarTienda(td);
+            rowsAffected += catalogoDAO.insertarCatalogo(td.getCatalogo());
         }
 
         return rowsAffected;
@@ -209,6 +211,195 @@ public class FileManager {
         }
 
         return rows;
+
+    }
+
+    private int cargarPedidos() {
+
+        PedidoDAO pedidoDAO = new PedidoDAO();
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+        ListadoProductosDAO listadoProductosDAO = new ListadoProductosDAO();
+        int rowsAffected = 0;
+
+        ArrayList<Pedido> pedidos = new ArrayList<>();
+        JsonNode pedidosNode = rootNode.get("pedidos");
+
+        for (JsonNode pedidoNode : pedidosNode) {
+
+            int codigo = pedidoNode.get("id").asInt();
+            //int tienda = pedidoNode.get("tienda").asInt();
+            int usuario = pedidoNode.get("usuario").asInt();
+            Usuario usuarioSol = new Usuario(usuario);
+            usuarioSol = usuarioDAO.buscarUsuario(usuarioSol, "DEPENDIENTES");
+            String fecha = pedidoNode.get("fecha").asText();
+            double total = pedidoNode.get("total").asDouble();
+            String estado = pedidoNode.get("estado").asText();
+            Pedido pedido = new Pedido(codigo, fecha, total, estado, usuarioSol);
+            pedido.crearLista();
+            JsonNode listadoProductosNode = pedidoNode.get("productos");
+
+            for (JsonNode productoNode : listadoProductosNode) {
+                int codigoPd = productoNode.get("codigo").asInt();
+                double precio = productoNode.get("costoU").asDouble();
+                int cantidad = productoNode.get("cantidad").asInt();
+                double precioTotal = productoNode.get("costoTotal").asDouble();
+
+                pedido.agregarAlListado(new Producto(codigoPd, precio, cantidad));
+
+            }
+
+            pedidos.add(pedido);
+
+        }
+        for (Pedido pd : pedidos) {
+
+            rowsAffected += pedidoDAO.insertarPedido(pd);
+            rowsAffected += listadoProductosDAO.insertarProducto(pd.getListadoProductos());
+        }
+
+        return rowsAffected;
+
+    }
+
+    private int cargarEnvios() {
+
+        EnvioDAO envioDAO = new EnvioDAO();
+        TiendaDAO tiendaDAO = new TiendaDAO();
+        ListadoProductosDAO listadoProductosDAO = new ListadoProductosDAO();
+        int rowsAffected = 0;
+
+        ArrayList<Envio> envios = new ArrayList<>();
+        JsonNode enviosNode = rootNode.get("envios");
+
+        for (JsonNode envioNode : enviosNode) {
+
+            int codigo = envioNode.get("id").asInt();
+            int tienda = envioNode.get("tienda").asInt();
+            //int usuario = envioNode.get("usuario").asInt();
+            Tienda tiendaDest = new Tienda(tienda);
+            tiendaDest = tiendaDAO.buscarTienda(tiendaDest);
+            String fechaSalida = envioNode.get("fechaSalida").asText();
+            String fechaRecibido = envioNode.get("fechaRecibido").asText();
+            double total = envioNode.get("total").asDouble();
+            String estado = envioNode.get("estado").asText();
+            Envio envio = new Envio(codigo, fechaSalida, fechaRecibido, estado, total, tienda, tiendaDest);
+            envio.crearLista();
+            JsonNode listadoProductosNode = envioNode.get("productos");
+
+            for (JsonNode productoNode : listadoProductosNode) {
+                int codigoPd = productoNode.get("codigo").asInt();
+                double precio = productoNode.get("costoU").asDouble();
+                int cantidad = productoNode.get("cantidad").asInt();
+                double precioTotal = productoNode.get("costoTotal").asDouble();
+
+                envio.agregarAlListado(new Producto(codigoPd, precio, cantidad));
+
+            }
+
+            envios.add(envio);
+
+        }
+        for (Envio envio : envios) {
+
+            rowsAffected += envioDAO.insertarEnvio(envio);
+            rowsAffected += listadoProductosDAO.insertarProducto(envio.getListadoEnvio());
+        }
+
+        return rowsAffected;
+    }
+
+    private int cargarDevoluciones() {
+
+        DevolucionDAO devolucionDAO = new DevolucionDAO();
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+        ReclamoDevolucionDAO reclamoDevolucionDAO = new ReclamoDevolucionDAO();
+        int rowsAffected = 0;
+
+        ArrayList<Devolucion> devoluciones = new ArrayList<>();
+        JsonNode devolucionesNode = rootNode.get("devoluciones");
+
+        for (JsonNode devolucionNode : devolucionesNode) {
+
+            int codigo = devolucionNode.get("id").asInt();
+            //int tienda = devolucionNode.get("tienda").asInt();
+            int usuario = devolucionNode.get("usuario").asInt();
+            Usuario encargado = new Usuario(usuario);
+            encargado = usuarioDAO.buscarUsuario(encargado, "BODEGUEROS");
+
+            String fecha = devolucionNode.get("fecha").asText();
+            double total = devolucionNode.get("total").asDouble();
+            String estado = devolucionNode.get("estado").asText();
+            Devolucion devolucion = new Devolucion(codigo, fecha, estado, total, encargado);
+            JsonNode listadoProductosNode = devolucionNode.get("productos");
+            devolucion.crearReclamo();
+            for (JsonNode productoNode : listadoProductosNode) {
+                int codigoPd = productoNode.get("codigo").asInt();
+                double precio = productoNode.get("costo").asDouble();
+                int cantidad = productoNode.get("cantidad").asInt();
+                double precioTotal = productoNode.get("costoTotal").asDouble();
+                String motivo = productoNode.get("motivo").asText();
+                devolucion.agregarProducto(new Producto(codigoPd, precio, cantidad));
+                devolucion.getReclamoDevolucion().setMotivoDevolucion(motivo);
+
+            }
+
+            devoluciones.add(devolucion);
+
+        }
+        for (Devolucion devolucion : devoluciones) {
+
+            rowsAffected += devolucionDAO.insertarDevolucion(devolucion);
+            rowsAffected += reclamoDevolucionDAO.insertarReclamo(devolucion.getReclamoDevolucion());
+        }
+
+        return rowsAffected;
+
+    }
+
+    private int cargarIncidencias() {
+
+        IncidenciaDAO incidenciasDAO = new IncidenciaDAO();
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+        ReclamoIncidenciaDAO reclamoIncidenciaDAO = new ReclamoIncidenciaDAO();
+        int rowsAffected = 0;
+
+        ArrayList<Incidencia> incidencias = new ArrayList<>();
+        JsonNode incidenciasNode = rootNode.get("incidencias");
+
+        for (JsonNode incidenciaNode : incidenciasNode) {
+
+            int codigo = incidenciaNode.get("id").asInt();
+            //int tienda = devolucionNode.get("tienda").asInt();
+            int usuario = incidenciaNode.get("usuario").asInt();
+            Usuario encargado = new Usuario(usuario);
+            encargado = usuarioDAO.buscarUsuario(encargado, "BODEGUEROS");
+
+            String fechaSalida = incidenciaNode.get("fecha").asText();
+            String estado = incidenciaNode.get("estado").asText();
+            String solucion = incidenciaNode.get("solucion").asText();
+            Incidencia incidencia = new Incidencia(codigo, fechaSalida, estado, encargado);
+            JsonNode listadoProductosNode = incidenciaNode.get("productos");
+            incidencia.setSolucion(solucion);
+            incidencia.crearReclamo();
+            for (JsonNode productoNode : listadoProductosNode) {
+                int codigoPd = productoNode.get("codigo").asInt();
+                int cantidad = productoNode.get("cantidad").asInt();
+                String motivo = productoNode.get("motivo").asText();
+                incidencia.agregarProducto(new Producto(codigoPd, cantidad));
+                incidencia.getReclamoIncidencia().setMotivoIncidencia(motivo);
+
+            }
+
+            incidencias.add(incidencia);
+
+        }
+        for (Incidencia incidencia : incidencias) {
+
+            rowsAffected += incidenciasDAO.insertarIncidencia(incidencia);
+            rowsAffected += reclamoIncidenciaDAO.incertarReclamo(incidencia.getReclamoIncidencia());
+        }
+
+        return rowsAffected;
 
     }
 
